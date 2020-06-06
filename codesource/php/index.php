0a6b12f19acc845cf3575f71194dfcd81e09fce0
tahaettabti <tahaettabti1234@gmail.com>
2020-06-06 01:06:19 +0100

    finish

---
+++ codesource/php/index.php
@@ -0,0 +1,141 @@
+<?php
+    include('menu.php');
+    include('header.php');
+    echo '
+    <style>
+      #carouselExampleControls {
+        margin-top: 2%;
+      }
+      .cardProduit {
+        margin-bottom: 0px;
+      }
+    </style>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img style="height:500px;" class="d-block w-100" src="../images/slide6.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+    <img style="height:500px;" class="d-block w-100" src="../images/slide5.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+    <img style="height:500px;" class="d-block w-100" src="../images/slide4.jpg" alt="Third slide">
+    </div> 
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a>
+    </div>';
+
+  
+      echo '<div class="divstandard">';
+      $conn = new mysqli("localhost", "root", "", "vente");
+      // Check connection
+      if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+      }
+  
+      $sql = "SELECT * FROM `produit` where idCategorie='1' limit 3";
+      $result = mysqli_query($conn, $sql);  
+                    
+      if ($result->num_rows > 0) {
+          echo '<div class="row">';
+          while($row = mysqli_fetch_array($result))  
+                  {  
+                      echo '<div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="data:image/jpeg;base64,'.base64_encode($row['imageProduit'] ).'" class="card-img-top" height="300" />  
+                      <div class="card-body">
+                          <h5 class="card-title">'.$row['nomProduit'].'</h5>
+                          <p class="card-text text-right">'.$row['prix'].' Dh</p>
+                          <a href="produit.php?id='.$row["idProduit"].'"><button type="button" name="ajoutePanier" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div>';  
+                  }
+          echo '</div>';
+      } 
+      $conn->close();
+      echo '</div>';
+    
+?>
+      <div class="divstandard">
+          <div class="row">
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/img22.jpg" class="card-img-top" height="300"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">HP PC Portable</h5>
+                          <p class="card-text text-right">8000 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/phone2.jpg" class="card-img-top" height="300"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">Samsung Galaxy S10</h5>
+                          <p class="card-text text-right">6000 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/img33.jpg" class="card-img-top" height="300" width="500"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">DELL PC Portable</h5>
+                          <p class="card-text text-right">6000 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+          </div>
+      </div>    
+      <div class="divstandard">
+          <div class="row">
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/cuisine3.jpg" class="card-img-top" height="300"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">Chalumeau de cuisine</h5>
+                          <p class="card-text text-right">300 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/fix2.jpg" class="card-img-top" height="300"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">MEISTER Mallette 60 pièces</h5>
+                          <p class="card-text text-right">350 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+                      <div class="col-sm-4 cardProduit">
+                      <div class="card">
+                      <img src="../images/panier1.png" class="card-img-top" height="300" width="500"/>  
+                      <div class="card-body">
+                          <h5 class="card-title">Panier Couscous</h5>
+                          <p class="card-text text-right">188 Dh</p>
+                          <a href="produit.php"><button type="button" name="" class="btn btn-light btn-lg">Consulter au Produit</button></a>
+                      </div>
+                      </div>
+                      </div> 
+          </div>
+      </div>    
+
+
+<?php 
+        
+  include('footer.php');
+
+?>
---
+++ codesource/php/produit.php
@@ -0,0 +1,110 @@
+<?php
+  include('menu.php');
+
+  echo '
+  <style>
+    #carouselExampleControls {
+      margin-top: 2%;
+    }
+    .search {
+      margin-top: 2%;
+      margin-left: 30%;
+    }
+    #input {
+      width: 30%;
+    } 
+  </style>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+   <div class="carousel-inner">
+     <div class="carousel-item active">
+       <img style="height:500px;" class="d-block w-100" src="../images/slide1.jpg" alt="First slide">
+      </div>
+     <div class="carousel-item">
+       <img style="height:500px;" class="d-block w-100" src="../images/slide2.jpg" alt="Second slide">
+     </div>
+     <div class="carousel-item">
+       <img style="height:500px;" class="d-block w-100" src="../images/slide3.jpg" alt="Third slide">
+     </div> 
+   </div>
+   <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+     <span class="sr-only">Previous</span>
+   </a>
+   <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+     <span class="sr-only">Next</span>
+   </a>
+ </div>';
+
+ echo '<div class="search">
+ <form action="" method="post" class="form-inline my-2 my-lg-0">
+ <input  name="requete" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id="input">
+ <button class="btn btn-light btn-lg" type="submit">Search</button>
+</form>
+</div>'; 
+ 
+    echo '<div class="divstandard">';
+    $conn = new mysqli("localhost", "root", "", "vente");
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    if ( isset($_POST['requete']) ) 
+        $requete = htmlentities($conn->real_escape_string($_POST['requete']));
+        
+    if (!empty($requete)) {
+        $req = "SELECT * FROM produit WHERE nomProduit LIKE '%$requete%'"; 
+        $exec = $conn->query($req);                            
+        // exécuter la requête
+        $nb_resultats = $exec->num_rows;  // compter les résultats
+
+      echo '<div class="row">';
+      while($donnees = mysqli_fetch_array($exec))  
+              {  
+                  echo '<div class="col-sm-4 cardProduit">
+                  <div class="card">
+                  <img src="data:image/jpeg;base64,'.base64_encode($donnees['imageProduit'] ).'" class="card-img-top" height="300"/>  
+                  <div class="card-body">
+                      <h5 class="card-title">'.$donnees['nomProduit'].'</h5>
+                      <p class="card-text text-right">'.$donnees['prix'].' Dh</p>
+                      <a href="panier.php?id='.$donnees["idProduit"].'"><button type="button" name="ajoutePanier" class="btn btn-light btn-lg">Ajouter au panier</button></a>
+                  </div>
+                  </div>
+                  </div>';  
+              } // fin de la boucle
+      echo '</div>';
+ 
+  }  else {
+    $sql = "SELECT * FROM `produit` ORDER BY `produit`.`idProduit` ASC ";
+    $result = mysqli_query($conn, $sql);  
+                  
+    if ($result->num_rows > 0) {
+        echo '<div class="row">';
+        while($row = mysqli_fetch_array($result))  
+                {  
+                    echo '<div class="col-sm-4 cardProduit">
+                    <div class="card">
+                    <img src="data:image/jpeg;base64,'.base64_encode($row['imageProduit'] ).'" class="card-img-top" height="300"/>  
+                    <div class="card-body">
+                        <h5 class="card-title">'.$row['nomProduit'].'</h5>
+                        <p class="card-text text-right">'.$row['prix'].' Dh</p>
+                        <a href="panier.php?id='.$row["idProduit"].'"><button type="button" name="ajoutePanier" class="btn btn-light btn-lg">Ajouter au panier</button></a>
+                    </div>
+                    </div>
+                    </div>';  
+                }
+        echo '</div>';
+ 
+    } 
+    else {
+    echo '<p class="text-center font-weight-bolder">Aucun Produit</p>';
+    }
+  } 
+    $conn->close();
+    echo '</div>';
+    include('footer.php');
+
+ ?>  
+
+
